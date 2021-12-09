f618cba95f3df442638554e3039d4f77662d0a73
Izuho <72969994+Izuho@users.noreply.github.com>
2021-12-10 06:12:14 +0900

    組み合わせ回路に

--- fabs/fabs.v
+++ fabs/fabs.v
@@ -1,24 +1,9 @@
-`timescale 1ns / 100ps
-`default_nettype none
-module fabs
-    (input wire [31:0] op,
-    output reg [31:0] result,
-    output reg ready,
-    input wire reset,
-    input wire clk
-    );
+module fabs(
+    input wire [31:0] op,
+    output wire [31:0] result
+);
 
-    always @(posedge clk) begin
-        if (~reset) begin
-            result <= 32'd0;
-            ready <= 1'b0;
-        end else begin
-           if (ready) begin
-                ready <= 1'b0;
-            end
-            result <= {1'b0, op[30:0]};
-            ready <= 1'b1; 
-        end
-    end
+assign result[31] = 1'b0;
+assign result[30:0] = op[30:0];
+
 endmodule
-`default_nettype wire
