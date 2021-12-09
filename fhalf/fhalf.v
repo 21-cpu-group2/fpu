30f53f1e61e943b0dee78a50c25e6e4bad9fbbc1
Izuho <72969994+Izuho@users.noreply.github.com>
2021-12-10 06:13:31 +0900

    組み合わせ回路に

--- fhalf/fhalf.v
+++ fhalf/fhalf.v
@@ -1,42 +1,13 @@
-`timescale 1ns / 100ps
-`default_nettype none
-module fhalf
-    (input wire [31:0] op,
-    output reg [31:0] result,
-    input wire clk,
-    output reg ready,
-    output reg valid,
-    input wire reset
-    );
+module fhalf(
+    input wire [31:0] op,
+    output reg [31:0] result
+);
 
     wire [7:0] exp = op[30:23];
     wire [7:0] exp_sub1 = (op[30:23] - 8'b1);
     wire [22:0] fra_shift = op[22:0] >> 1;
 
-    always @(posedge clk) begin
-        if (~reset) begin
-            result <= 32'b0;
-            ready <= 1'b0;
-            valid <= 1'b0;
-        end
-        if (ready) begin
-            ready <= 1'b0;
-            valid <= 1'b0;
-        end
-        if (exp == 8'b0) begin
-            result <= {op[31:23], fra_shift};
-            ready <= 1'b1;
-            valid <= 1'b1;
-        end else if (exp == 8'b1) begin
-            result <= {op[31], 8'b0, 1'b1, fra_shift[21:0]};
-            ready <= 1'b1;
-            valid <= 1'b1;
-        end else begin
-            result <= {op[31], exp_sub1, fra_shift};
-            ready <= 1'b1;
-            valid <= 1'b1;
-        end
-    end
+    assign result = (exp == 8'b0)? {op[31:23], fra_shift} :
+                    (exp == 8'b1)? {op[31], 8'b0, 1'b1, fra_shift[21:0]} : {op[31], exp_sub1, fra_shift};
 
 endmodule
-`default_nettype wire
