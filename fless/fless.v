d391d080ab3e2a041365ef52fe09af003757d852
Izuho <72969994+Izuho@users.noreply.github.com>
2021-12-10 06:14:22 +0900

    組み合わせ回路に

--- fless/fless.v
+++ fless/fless.v
@@ -1,28 +1,15 @@
-`timescale 1ns / 100ps
-`default_nettype none
-module fless
-    (input wire [31:0] op1,
+module fless (
+    input wire [31:0] op1,
     input wire [31:0] op2,
-    output reg result,
-    input clk
-    );
+    output wire result
+);
 
     wire sig1 = op1[31];
     wire sig2 = op2[31];
     wire exp1 = op1[30:23];
     wire exp2 = op2[30:23];
 
-    always @(posedge clk) begin
-        if (sig1 == sig2) begin
-            if (exp1 == exp2) begin
-                result <= (op1[22:0] < op2[22:0]);
-            end else begin
-                result <= (exp1 < exp2);
-            end
-        end else begin
-            result <= sig1;
-        end
-    end
+    assign result = (sig1 == sig2 && exp1 == exp2)? (op1[22:0] < op2[22:0]) :
+                    (sig1 == sig2)? (exp1 < exp2) : sig1;
 
 endmodule
-`default_nettype wire
