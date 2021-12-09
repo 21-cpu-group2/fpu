50396f47095c1d9a9ae5e6d37c0ca8bdab4b892e
Izuho <72969994+Izuho@users.noreply.github.com>
2021-12-10 06:16:57 +0900

    組み合わせ回路に

--- fneg/fneg.v
+++ fneg/fneg.v
@@ -1,19 +1,9 @@
-`timescale 1ns / 100ps
-`default_nettype none
-module fneg
-    (input wire [31:0] op,
-    output reg [31:0] result,
-    input wire clk,
-    input wire reset
-    );
+module fneg(
+    input wire [31:0] op,
+    output wire [31:0] result
+);
 
-    always @(posedge clk) begin
-        if (~reset) begin
-            result <= 32'd0;
-        end else begin
-            result <= {~op[31], op[30:0]};
-        end
-    end
+assign result[31] = ~op[31];
+assign result[30:0] = op[30:0];
 
 endmodule
-`default_nettype wire
