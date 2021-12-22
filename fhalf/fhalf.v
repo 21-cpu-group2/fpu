567db7f10069cbc4a60ae5685cd42485224592cb
Laplace-LT <makoto@akiyamashinnoMacBook-Air.local>
2021-12-22 22:00:34 +0900

    fhalf修正

--- fhalf/fhalf.v
+++ fhalf/fhalf.v
@@ -8,6 +8,6 @@ module fhalf(
     wire [22:0] fra_shift = op[22:0] >> 1;
 
     assign result = (exp == 8'd0)? {op[31:23], fra_shift} :
-                    (exp == 8'd1)? {op[31], 8'd0, 1'b1, fra_shift[21:0]} : {op[31], exp_sub1, fra_shift};
+                    (exp == 8'd1)? {op[31], 8'd0, 1'b1, op[22:1]} : {op[31], exp_sub1, op[22:0]};
 
 endmodule
