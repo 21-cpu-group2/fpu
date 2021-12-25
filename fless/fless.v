63c6940ffbe189aecc7de0b61252046eee9366e9
Laplace-LT <makoto.akiyama.0228@gmail.com>
2021-12-25 15:12:25 +0900

    fless修正

--- fless/fless.v
+++ fless/fless.v
@@ -6,8 +6,8 @@ module fless (
 
     wire sig1 = op1[31];
     wire sig2 = op2[31];
-    wire exp1 = op1[30:23];
-    wire exp2 = op2[30:23];
+    wire [7:0] exp1 = op1[30:23];
+    wire [7:0] exp2 = op2[30:23];
 
     assign result = ((sig1 == sig2) && (exp1 == exp2)) ? (op1[22:0] < op2[22:0]) ^ sig1 :
                     (sig1 == sig2) ? (exp1 < exp2) ^ sig1 : sig1;
