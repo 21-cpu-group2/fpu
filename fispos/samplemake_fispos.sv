2ead756a43efa9c05e4635867127103b82128310
Laplace-LT <makoto.akiyama.0228@gmail.com>
2022-01-03 16:23:57 +0900

    fisposのsample作成

---
+++ fispos/samplemake_fispos.sv
@@ -0,0 +1,32 @@
+`timescale 1us / 100ns
+`default_nettype none
+
+module samplemake_fispos();
+    wire [31:0] op;
+    logic result;
+    logic [31:0] oplogic;
+
+    int i;
+    bit sig;
+    bit [22:0] fra;
+    bit [7:0] exp;
+
+    bit [31:0] dum;
+
+   assign op = oplogic;
+   
+   fispos f(op, result);
+
+   initial begin
+        oplogic = 32'd0;
+        #1;
+        $display("%b %b", oplogic, result);//ゼロ
+        for (i = 0; i < 10000; i++) begin
+            oplogic = $urandom();
+            #1;
+            $display("%b %b", oplogic, result);//ランダム
+        end
+        $finish;
+   end
+endmodule
+`default_nettype wire
